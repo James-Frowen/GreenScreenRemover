3a1ea2a1d76b591319219c63b703368c27a65129
JamesFrowen-PC\JamesFrowen <jamesfrowendev@gmail.com>
2018-09-12 19:39:03 +0100

    intial commit simple green background remover

---
+++ GreenScreenRemover/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+
+namespace GreenScreenRemover
+{
+    class Program
+    {
+        private const string SOURCE_PATH = "./source.png";
+        private const string RESULT_PATH = "./result.png";
+
+        public static void Main(string[] args)
+        {
+            string sourcePath = SOURCE_PATH;
+            string resultPath = RESULT_PATH;
+
+            if (args.Length >= 1)
+            {
+                sourcePath = args[0];
+            }
+            if (args.Length >= 2)
+            {
+                resultPath = args[1];
+            }
+
+            removeBackground(sourcePath, resultPath);
+        }
+
+        private static void removeBackground(string sourcePath, string resultPath, byte threshold = 10)
+        {
+            Bitmap source = null;
+            Bitmap result = null;
+            try
+            {
+                source = new Bitmap(sourcePath);
+                result = new Bitmap(source);
+                for (int x = 0; x < source.Width; x++)
+                {
+                    for (int y = 0; y < source.Height; y++)
+                    {
+                        var color = source.GetPixel(x, y);
+                        if (color.G > color.R + threshold
+                        && color.G > color.B + threshold)
+                        {
+                            color = Color.Transparent;
+                        }
+
+                        result.SetPixel(x, y, color);
+                    }
+                }
+                result.Save(resultPath, ImageFormat.Png);
+            }
+            finally
+            {
+                if (source != null)
+                {
+                    source.Dispose();
+                }
+                if (result != null)
+                {
+                    result.Dispose();
+                }
+            }
+        }
+    }
+}
